9aa028e687d89c0e805d4b13b56a1e3c9c040594
Ayanzino <102111862+Ayanzino@users.noreply.github.com>
2023-06-26 23:33:08 -0700

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"649a80f7098f07001997972a",
+				"649a80f7098f07001997972d"
+			]
+		},
+		"649a80f7098f07001997972a": {
+			"id": "649a80f7098f07001997972a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"649a80f7098f07001997972d": {
+			"id": "649a80f7098f07001997972d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Input, Button, Section } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml, Formspree } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section background="--color-light" color="--dark" padding="64px 0 64px 0">
+			<Box margin="-16px -16px -16px -16px" display="flex" flex-wrap="wrap">
+				<Box padding="16px 16px 16px 16px" width="50%" lg-width="100%">
+					<Box>
+						<Text
+							font="--base"
+							color="--grey"
+							letter-spacing="1px"
+							text-transform="uppercase"
+							margin="0"
+						>
+							Contact
+						</Text>
+						<Text font="--headline2" max-width="500px" margin="10px 0 0 0">
+							Email us, we would love to hear form you
+						</Text>
+					</Box>
+				</Box>
+				<Box width="50%" padding="8px 8px 8px 8px" lg-width="100%">
+					<Box>
+						<Formspree endpoint="xeqpgrlv">
+							<Box
+								gap="16px"
+								display="grid"
+								flex-direction="row"
+								flex-wrap="wrap"
+								grid-template-columns="repeat(2, 1fr)"
+								grid-gap="16px"
+							>
+								<Box sm-width="100%" display="flex" flex-direction="column">
+									<Text font="--base" margin="0 0 4px 0">
+										Name
+									</Text>
+									<Input width="100%" name="name" type="text" />
+								</Box>
+								<Box sm-width="100%" display="flex" flex-direction="column">
+									<Text font="--base" margin="0 0 4px 0">
+										Email
+									</Text>
+									<Input width="100%" type="email" name="email" />
+								</Box>
+								<Box display="flex" flex-direction="column" align-items="flex-start" grid-column="1 / span 2">
+									<Button>
+										Send
+									</Button>
+								</Box>
+							</Box>
+						</Formspree>
+					</Box>
+				</Box>
+			</Box>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"649a80f7098f070019979723"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
